2d2429af3517988a9c0049eb08a8358225d80b05
nigeldouglas-itcarlow <C00292053@itcarlow.ie>
2023-04-25 19:03:40 +0200

    added comments for code review and removed unused packages - urllib.parse and tornado.IOLoop

--- test/registration.py
+++ test/registration.py
@@ -1,21 +1,24 @@
+# Import necessary modules
 from json import dumps
 from tornado.escape import json_decode
-from tornado.ioloop import IOLoop
 from tornado.web import Application
 
+# Import RegistrationHandler from api.handlers.registration module
 from api.handlers.registration import RegistrationHandler
 
+# Import BaseTest class from current package
 from .base import BaseTest
 
-import urllib.parse
-
+# Define RegistrationHandlerTest as a subclass of BaseTest
 class RegistrationHandlerTest(BaseTest):
 
+    # Define setUpClass method to set up a test Application
     @classmethod
     def setUpClass(self):
         self.my_app = Application([(r'/registration', RegistrationHandler)])
         super().setUpClass()
 
+    # Define a test for registration with a full set of input data
     def test_registration(self):
         email = 'test@test.com'
         full_name = 'testFullName'
@@ -23,6 +26,7 @@ class RegistrationHandlerTest(BaseTest):
         disability_type = 'testDisability'
         display_name = 'testDisplayName'
 
+        # Set up the request body with input data
         body = {
           'email': email,
           'fullName': full_name,
@@ -32,9 +36,13 @@ class RegistrationHandlerTest(BaseTest):
           'displayName': display_name
         }
 
+        # Send a POST request to the /registration endpoint with the input data
         response = self.fetch('/registration', method='POST', body=dumps(body))
+
+        # Assert that the response code is 200 (OK)
         self.assertEqual(200, response.code)
 
+        # Parse the response body as JSON and assert that the output data matches the input data
         body_2 = json_decode(response.body)
         self.assertEqual(email, body_2['email'])
         self.assertEqual(full_name, body_2['fullName'])
@@ -42,30 +50,44 @@ class RegistrationHandlerTest(BaseTest):
         self.assertEqual(disability_type, body_2['disability'])
         self.assertEqual(display_name, body_2['displayName'])
 
+    # Define a test for registration without a display name
     def test_registration_without_display_name(self):
         email = 'test@test.com'
 
+        # Set up the request body with input data (excluding display name)
         body = {
           'email': email,
           'password': 'testPassword'
         }
 
+        # Send a POST request to the /registration endpoint with the input data
         response = self.fetch('/registration', method='POST', body=dumps(body))
+
+        # Assert that the response code is 200 (OK)
         self.assertEqual(200, response.code)
 
+        # Parse the response body as JSON and assert that the display name is set to the email address
         body_2 = json_decode(response.body)
         self.assertEqual(email, body_2['email'])
         self.assertEqual(email, body_2['displayName'])
 
+    # Define a test for attempting to register the same user twice
     def test_registration_twice(self):
+        # Set up the request body with input data
         body = {
           'email': 'test@test.com',
           'password': 'testPassword',
           'displayName': 'testDisplayName'
         }
 
+        # Send a POST request to the /registration endpoint with the input data
         response = self.fetch('/registration', method='POST', body=dumps(body))
+
+        # Assert that the response code is 200 (OK)
         self.assertEqual(200, response.code)
 
+        # Attempt to register the same user again with the same input data
         response_2 = self.fetch('/registration', method='POST', body=dumps(body))
+
+        # Assert that the response code is 409 (Conflict), indicating that the user already exists
         self.assertEqual(409, response_2.code)
