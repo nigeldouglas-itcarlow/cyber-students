308c648bb2aa2c574f88832d4a2a6defece4ddae
nigeldouglas-itcarlow <C00292053@itcarlow.ie>
2023-04-29 16:10:19 +0100

    I left comments explaining how the test scenario connects to a Mongo backend, how I created a Fernet object for encrypting and decrypting data, how I created the PasswordHasher object for hashing and verifying passwords, and finally how I defined methods for encrypting, decrypting, and the hashing data. By default, Martin defined specific methods for setting up and tearing down the DB before and after each test.

--- test/base.py
+++ test/base.py
@@ -1,3 +1,4 @@
+# Import necessary libraries
 from argon2 import PasswordHasher
 from concurrent.futures import ThreadPoolExecutor
 from cryptography.fernet import Fernet
@@ -5,47 +6,57 @@ from motor import MotorClient
 from tornado.ioloop import IOLoop
 from tornado.testing import AsyncHTTPTestCase
 
+# Import configuration parameters from the conf.py file
 from .conf import MONGODB_HOST, MONGODB_DBNAME, WORKERS
 
 
+# Create a base test case class that inherits from AsyncHTTPTestCase
 class BaseTest(AsyncHTTPTestCase):
 
+    # Class method that is run once at the beginning of all tests
     @classmethod
     def setUpClass(self):
+        # Connect to the MongoDB database specified in the configuration
         self.my_app.db = MotorClient(**MONGODB_HOST)[MONGODB_DBNAME]
 
+        # Create a thread pool executor with the number of workers specified in the configuration
         self.my_app.executor = ThreadPoolExecutor(WORKERS)
-        # This is how I generated the key
-        # I ran this from shell when the key needs recycling
-        # key = Fernet.generate_key()
-        # Same as app.py this key is presented in plaintext
+
+        # Generate a Fernet key or use a pre-generated one from configuration
         key = b'OfegPNE8TI_tXLoPA4iC3ibJeU-xrVFsmg0VYUgNh9I='
-        # This package was included in the requirements.txt file
-        # It offers a simple way of encrypting the data
+
+        # Create a Fernet object using the key to encrypt and decrypt data
         self.my_app.fernet = Fernet(key)
-        # I added PasswordHasher as it was a highly-recommended hasher
-        # This simply initiates the hasher
+
+        # Create a PasswordHasher object to hash and verify passwords
         self.my_app.password_hasher = PasswordHasher()
 
+    # Method that returns the current IOLoop
     def get_new_ioloop(self):
         return IOLoop.current()
 
+    # Method that returns the current Tornado application object
     def get_app(self):
         return self.my_app
 
+    # Method that decrypts a given value using the Fernet object
     def decrypt(self, value):
         return self.my_app.fernet.decrypt(value).decode()
 
+    # Method that encrypts a given value using the Fernet object
     def encrypt(self, value):
         return self.my_app.fernet.encrypt(str.encode(value, encoding="utf8"))
 
+    # Method that hashes a given value using the PasswordHasher object
     def hash(self, value):
         return self.my_app.password_hasher.hash(value)
 
+    # Method that is run before each test to drop the users collection in the database
     def setUp(self):
         super().setUp()
         self.get_app().db.users.drop()
 
+    # Method that is run after each test to drop the users collection in the database
     def tearDown(self):
         super().tearDown()
         self.get_app().db.users.drop()
