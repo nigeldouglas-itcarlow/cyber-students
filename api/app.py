5859fc55c65f2f341b2c8a42b0ff3b686b4072c2
nigeldouglas-itcarlow <C00292053@itcarlow.ie>
2023-04-29 22:17:06 +0100

    The time_cost is the number of iterations of the algorithm, which determines how much time is needed to compute the hash. Increasing this value makes the algorithm slower and more secure against brute-force attacks, but also increases the time required to compute a hash.
    The memory_cost is the amount of memory used by the algorithm, which determines how much memory is required to compute the hash. Increasing this value makes the algorithm more secure against memory-based attacks, but also increases the memory required to compute a hash.
    
    parallelism refers to the number of threads used by the algorithm, which determines how many parallel threads are used to compute the hash. Increasing this value makes the algorithm faster, but also increases the risk of side-channel attacks.
    
    the hash_len is the length of the output hash in bytes. Increasing this value makes the output hash longer and more secure against collisions.
    
    Whereas, the salt_len is the length of the salt used in the hash in bytes. Increasing this value makes it more difficult for attackers to generate pre-computed tables or rainbow tables that can be used to reverse engineer the hash.
    
    The values used for each of these parameters should be chosen based on the desired trade-off between security and performance. Higher values generally result in more secure hashes, but at the cost of longer computation times and increased memory usage.

--- api/app.py
+++ api/app.py
@@ -34,6 +34,21 @@ class Application(Application):
 
         # Set up the encryption, password hashing, and thread pool components
         self.fernet = Fernet(key)
+        # Here is a stronger hashing process using the argon2id algorithm with tuned parameters.
+        # argon2id algorithm is a hybrid of argon2i and argon2d algorithms, and is considered the most secure option for password hashing.
         self.password_hasher = argon2.PasswordHasher(
-            time_cost=4, memory_cost=102400, parallelism=8, hash_len=16, salt_len=16)
+          time_cost=16, memory_cost=65536, parallelism=8, hash_len=32, salt_len=16)
         self.executor = ThreadPoolExecutor(WORKERS)
+
+# I used a time cost of 16, which means that the algorithm will be run 2^16 times.
+# The memory cost of 65536 means the algorithm will use 64 MB of memory.
+# Finally, the hash length of 32 bytes will produce a longer and more secure hash.
+
+# Increasing the strength of hashing makes it more difficult to crack passwords using brute-force/dictionary attacks.
+# However, stronger hashing also means that it will take more time and resources to hash passwords
+# This will slow down the authentication process dramatically
+# Increasing strength of hashing requires more memory, which is a concern for systems with limited resources.
+
+# It's generally important to strike a balance between security and performance when choosing a hashing algorithm
+# I chose the most intensive parameters as this is simply a test lab environment
+# I kept in mind that hashing is only one part. I forced the use of strong passwords
